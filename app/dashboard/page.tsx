9545d42dd910af5ec0e48d8513a6c712603d150d
omegamaster86 <masya868686@gmail.com>
2025-05-05 16:38:09 +0900

    init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+const Dashboard = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard;
